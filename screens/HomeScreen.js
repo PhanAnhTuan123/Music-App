8b8f693cfd4105c7454a9858031f8a009c623d94
Tuan <quoctuan090103@gmail.com>
2024-12-02 12:00:29 +0700

    done home

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -10,7 +10,18 @@ import {
 } from "react-native";
 import { getHomePage } from "../apis/home";
 import { useDispatch } from "react-redux";
+import {
+    setAudioUrl,
+    setCurrentProgress,
+    setCurrentSongIndex,
+    setIsPlaying,
+    setPlayerData,
+    setPlaylist,
+    setRadioUrl,
+    setShowPlayer,
+} from "../redux-toolkit/playerSlice";
 import Header from "../modules/Search/Header";
+import { useAuth } from "../context/auth-context";
 import SkeletonContent from "react-native-skeleton-content";
 
 export default function HomeScreen({ navigation }) {
@@ -44,12 +55,11 @@ export default function HomeScreen({ navigation }) {
         fetchData();
     }, []);
 
-    // Hàm trả về lời chào phù hợp dựa trên giờ hiện tại
     const getCurrentTime = () => {
         const hour = new Date().toLocaleTimeString("vi-VN", {
             hour: "2-digit",
         });
-        // chuyển đổi giờ thành số
+        // convert hour to number
         const hourNumber = parseInt(hour.split(":")[0]);
         const str = "";
         if (hourNumber >= 0 && hourNumber < 12) {
@@ -63,7 +73,7 @@ export default function HomeScreen({ navigation }) {
         }
         return str;
     };
-    
+
     return (
         <ScrollView style={styles.container}>
             <SkeletonContent
@@ -244,7 +254,73 @@ export default function HomeScreen({ navigation }) {
                         data={homeData}
                         renderItem={({ item }) => (
                             <View style={styles.section}>
-                                
+                                <Text style={styles.sectionTitle}>
+                                    {item.title}
+                                </Text>
+                                <FlatList
+                                    horizontal={true}
+                                    data={
+                                        item.sectionType === "new-release"
+                                            ? item.items?.all || []
+                                            : item.items || []
+                                    }
+                                    renderItem={({ item }) => (
+                                        <Pressable
+                                            onPress={() => {
+                                                if (item.duration > 0) {
+                                                    dispatch(
+                                                        setCurrentProgress(0)
+                                                    );
+                                                    dispatch(
+                                                        setCurrentSongIndex(0)
+                                                    );
+                                                    dispatch(setPlaylist([]));
+                                                    dispatch(
+                                                        setPlayerData(item)
+                                                    );
+                                                    dispatch(setAudioUrl(""));
+                                                    dispatch(setRadioUrl(""));
+                                                    dispatch(
+                                                        setShowPlayer(true)
+                                                    );
+                                                    dispatch(
+                                                        setIsPlaying(true)
+                                                    );
+                                                } else {
+                                                    dispatch(
+                                                        setIsPlaying(false)
+                                                    );
+                                                    dispatch(
+                                                        setCurrentProgress(0)
+                                                    );
+                                                    navigation.navigate(
+                                                        "PlayList",
+                                                        {
+                                                            id: item.encodeId,
+                                                        }
+                                                    );
+                                                }
+                                            }}
+                                            style={styles.itemContainer}
+                                        >
+                                            <Image
+                                                source={{
+                                                    uri: item.thumbnailM,
+                                                }}
+                                                style={styles.itemImage}
+                                            />
+                                            <Text
+                                                numberOfLines={2}
+                                                style={styles.itemTitle}
+                                            >
+                                                {item.title}
+                                            </Text>
+                                        </Pressable>
+                                    )}
+                                    keyExtractor={(item, index) =>
+                                        index.toString()
+                                    }
+                                />
                             </View>
                         )}
                     />
@@ -259,6 +335,21 @@ const styles = StyleSheet.create({
         backgroundColor: "#121212",
         flex: 1,
         padding: 15,
+    },
+    header: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        paddingHorizontal: 20,
+        paddingVertical: 25,
+    },
+    headerText: {
+        color: "#fff",
+        fontSize: 16,
+        fontWeight: "700",
+    },
+    headerIcon: {
+        width: 23,
+        height: 23,
     },
     list: {
         flexDirection: "row",
@@ -287,16 +378,5 @@ const styles = StyleSheet.create({
         fontSize: 16,
         fontWeight: "bold",
         marginTop: 6,
-    },
-    sectionTitle: {
-        color: "#fff",
-        fontSize: 15,
-        fontWeight: "700",
-    },
-    itemContainer: {
-        paddingVertical: 8,
-        width: 168,
-        height: 200,
-        borderRadius: 4,
     },
 });
