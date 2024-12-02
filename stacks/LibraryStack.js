81a17c5facf031b39de1c9f6b7136ee51d6c505f
PhanAnhTuan123 <phantuan7a5@gmail.com>
2024-12-02 10:53:50 +0700

    feat(stack): Update stack

---
+++ stacks/LibraryStack.js
@@ -0,0 +1,88 @@
+import { StyleSheet, View } from "react-native";
+import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import LibraryScreen from "../screens/LibraryScreen";
+import ArtistListScreen from "../screens/ArtistListScreen";
+import Artist from "../modules/Playlist/Artist";
+import PlayList from "../modules/Playlist/PlayList";
+import CreatePlaylistScreen from "../screens/CreatePlaylistScreen";
+import AddSongToPlaylistScreen from "../screens/AddSongToPlaylistScreen";
+const Stack = createNativeStackNavigator();
+export default function LibraryStack() {
+    return (
+        <Stack.Navigator>
+            <Stack.Screen
+                name="LibraryHome"
+                component={LibraryScreen}
+                options={{ headerShown: false }}
+            />
+            <Stack.Screen
+                name="ArtistListScreen"
+                component={ArtistListScreen}
+                options={{ headerShown: false }}
+            />
+            <Stack.Screen
+                name="ArtistPage"
+                component={Artist}
+                options={{
+                    headerTitle: "",
+                    headerBackground: () => {
+                        return (
+                            <View
+                                style={{
+                                    background: "#0080AE",
+                                    flex: 1,
+                                }}
+                            ></View>
+                        );
+                    },
+                    headerTintColor: "white",
+                    headerShadowVisible: false,
+                }}
+            />
+            <Stack.Screen
+                name="PlayList"
+                component={PlayList}
+                options={{
+                    headerTitle: "",
+                    headerBackground: () => {
+                        return (
+                            <View
+                                style={{
+                                    background: "#0080AE",
+                                    flex: 1,
+                                }}
+                            ></View>
+                        );
+                    },
+                    headerTintColor: "white",
+                    headerShadowVisible: false,
+                }}
+            />
+            <Stack.Screen
+                name="CreatePlaylistScreen"
+                component={CreatePlaylistScreen}
+                options={{ headerShown: false }}
+            />
+            <Stack.Screen
+                name="AddSongToPlaylistScreen"
+                component={AddSongToPlaylistScreen}
+                options={{
+                    headerTitle: "Thêm vào danh sách phát này",
+                    headerBackground: () => {
+                        return (
+                            <View
+                                style={{
+                                    background: "#0080AE",
+                                    flex: 1,
+                                }}
+                            ></View>
+                        );
+                    },
+                    headerTintColor: "white",
+                    headerShadowVisible: false,
+                }}
+            />
+        </Stack.Navigator>
+    );
+}
